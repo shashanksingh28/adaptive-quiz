cd4119fc3ef61780e0a258a5a2b84c16813a63ae
dhiraj92 <dhiraj92@gmail.com>
2016-03-08 21:46:48 -0700

    added ask question in Question\ask url

--- AdaptiveQ/app.js
+++ AdaptiveQ/app.js
@@ -4,23 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var nodemailer = require("nodemailer");
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var Question = require('./routes/Question');
 var app = express();
 mongoose.connect("52.37.232.47/adaptq_dev");
 port = 3000;
-console.log('Server started! At http://localhost:' + port);
+console.log('Server has started! At http://localhost:' + port);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
 app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(nodemailer);
 app.use(logger('dev'));
-//app.use(bodyParser);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
--- AdaptiveQ/routes/Question.js
+++ AdaptiveQ/routes/Question.js
@@ -11,8 +11,23 @@ router.post('/', function(req, res){
   var ans = req.body.option;
   //check ans here 
   console.log(ans);
-  res.send(ans);
-  
+  res.send(ans);  
+  });
+
+router.get('/ask', function(req, res){
+	
+  res.render('askquestion', {Question : qn});
+  });
+
+router.post('/ask', function(req, res){
+	var question = req.body.question;
+	var op1 = req.body.option1;
+	var op2 = req.body.option2;
+	var op3 = req.body.option3;
+	var op4 = req.body.option4;
+	var ans = req.body.answer;
+  res.send(question + " " + ans);
   });
 
 module.exports = router;
+
