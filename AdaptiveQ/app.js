1800dac6c40feefbd3a0b799f2088c4552ba57ce
sriramoman <sriramoman@gmail.com>
2016-03-20 19:28:20 -0700

    Resolution
    Conflict

--- AdaptiveQ/app.js
+++ AdaptiveQ/app.js
@@ -60,6 +60,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
+/*if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -78,5 +79,6 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+*/
 
 module.exports = app;
