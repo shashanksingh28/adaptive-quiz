cea5d766eae47bee10c93e0d170afe4337b184ae
AjinxPatil <Ajinx.Patil@gmail.com>
2016-04-09 19:04:41 -0700

    Modified index field names.

--- creco/creco/src/main/java/com/adaptq/temp/Demo.java
+++ creco/creco/src/main/java/com/adaptq/temp/Demo.java
@@ -1,17 +1,19 @@
-package com.adaptq.creco;
+package com.adaptq.temp;
 
 import java.io.IOException;
 import java.text.ParseException;
 
 import org.apache.solr.client.solrj.SolrServerException;
 
+import com.adaptq.creco.Creco;
+
 public class Demo {
 	public static void main(String[] args)
 			throws IOException, ParseException, SolrServerException {
-		String text = "inheritance java classes object";
+		String query = "string";
 		Creco reco = new Creco("http://localhost:8983/solr/adaptq");
-		reco.crawlAndIndex();
-		String output = reco.get(text.replaceAll("[\\W]", " "));
+		//reco.crawlAndIndex();
+		String output = reco.search(query.replaceAll("[\\W]", " "));
 		System.out.println(output);
 	}
 }
