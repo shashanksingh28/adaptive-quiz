8db953562c5a9ed6aa93601a38bdb0a29ee4cc9e
Shashank Singh <shashanksingh28@gmail.com>
2016-06-25 15:41:39 -0700

    http post used in login

--- AdaptiveQ/public/js/loginApp.js
+++ AdaptiveQ/public/js/loginApp.js
@@ -4,7 +4,7 @@ loginApp.run(function($rootScope){
         $rootScope.form = 1;
 });
 
-loginApp.controller('loginController', function($scope, $rootScope){
+loginApp.controller('loginController',['$http','$scope','$rootScope', function($http, $scope, $rootScope){
         $scope.toRegister = function(){
                 $rootScope.form = 2;
         };
@@ -12,24 +12,33 @@ loginApp.controller('loginController', function($scope, $rootScope){
                 $rootScope.form = 3;
         };
 
-        $scope.error_msg = "";
-        $scope.user = {email: $scope.loginEmail,
-                       password: $scope.loginPassword};
+        $scope.model = {'email': '', 'password' : '' };
+        //$scope.loginEmail = "";
+        //$scope.error_msg = "";
+        //$scope.user = {email: $scope.loginEmail,
+        //               password: $scope.loginPassword};
 
         $scope.login = function(){
-                $scope.error_msg = "login button pressed";
-        }; 
-});
-        
+
+          console.log($scope.model);
+          $http.post('/login', $scope.model).then(function(successResponse){
+            $scope.error_msg = "Authenticated";
+            console.log(successResponse);
+          }, function(error){
+            $scope.error_msg = "Problem in connecting to server";
+          });
+        };
+}]);
+
 loginApp.controller('registerController', function($scope, $rootScope){
         $scope.toLogin = function(){
                 $rootScope.form = 1;
         };
 
         $scope.error_msg = "";
-        $scope.newuser = {accountType: $scope.accounttype, 
-                          name: $scope.registerName, 
-                          mail: $scope.registerEmail, 
+        $scope.newuser = {accountType: $scope.accounttype,
+                          name: $scope.registerName,
+                          mail: $scope.registerEmail,
                           password: $scope.registerPassword};
 
         $scope.register = function(){
@@ -44,7 +53,7 @@ loginApp.controller('recoveryController', function($scope, $rootScope){
 
         $scope.error_msg = "";
         $scope.recovery_msg = "";
-        
+
         $scope.recovery = function(){
                 $scope.recovery_msg = "recovery button pressed";
         };
