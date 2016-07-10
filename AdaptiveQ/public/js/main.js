4146f3bb532d100dede19bd7878dce0a7f723c05
chenbobby <appdev.bobchen@gmail.com>
2016-07-10 13:11:43 -0700

    create calender GUI in dashboard and create general GUI for question tab

--- AdaptiveQ/public/js/main.js
+++ AdaptiveQ/public/js/main.js
@@ -1,30 +1,37 @@
 var mainApp = angular.module('mainApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap']);
 
-mainApp.config(['$routeProvider', function($routeProvider){
-    /*
+mainApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
        $routeProvider
-       .when('/dashboard', {
-       templateUrl: 'dashboard.html'
+       .when('/', {
+       templateUrl: 'partials/dashboard',
+       controller: 'dashboardController'
        })
        .when('/question', {
-       templateUrl: 'question.html'
+       templateUrl: 'partials/question',
+       controller: 'questionController'
        })
        .when('/askquestion', {
-       templateUrl: 'askquestion.html'
+       templateUrl: 'partials/askquestion',
+       controller: 'askQuestionController'
        })
        .when('/classdata', {
-       templateUrl: 'classdata.html'
+       templateUrl: 'partials/classdata',
+       controller: 'classDataController'
        })
        .when('/myaccount', {
-       templateUrl: 'myaccount.html'
+       templateUrl: 'partials/myaccount',
+       controller: 'accountController'
        })
        .otherwise({
-       redirectTo: '/dashboard'
+       redirectTo: '/'
        });
-       */
+
+       $locationProvider.html5Mode(true);
 }]);
 
-mainApp.controller('mainController', ['$http', '$scope', function ($http, $scope) {
+mainApp.run(['$route', function(){}]);
+
+mainApp.controller('navController', ['$http', '$scope', function ($http, $scope) {
     $scope.message = "angularjs is connected";
 
     $scope.logout = function(){
@@ -40,4 +47,290 @@ mainApp.controller('mainController', ['$http', '$scope', function ($http, $scope
     };
 }]);
 
-//The routeProvider functionality apparently won't work unless it's run on a server...
+mainApp.controller('dashboardController', function($scope){
+
+    // -------- Question Panel --------
+    noquestion = {
+        date: '',
+        title: "-",
+        concepts: "",
+        status: 'No Question'
+    };
+
+    $scope.$watch('dt', function(){
+        console.log("Date has changed to " + $scope.dt);
+        var dayToCheck = new Date($scope.dt).setHours(0,0,0,0);
+
+        for (var i = 0; i < questions.length; i++) {
+            var currentDay = new Date(questions[i].date).setHours(0,0,0,0);
+
+            if (dayToCheck === currentDay) {
+                $scope.question = questions[i];
+                console.log("Date with Question Found");
+
+                switch($scope.question.status){
+                    case 'attemptedCorrect':
+                        $scope.prettyStatus = "Attempted Correctly";
+                        break;
+                    case 'attemptedIncorrect':
+                        $scope.prettyStatus = "Attempted Incorrectly";
+                        break;
+                    case 'unattempted':
+                        $scope.prettyStatus = "Not Yet Attempted";
+                        break;
+                    default:
+                        $scope.prettyStatus = "Status Not Found";
+                }
+
+                return true;
+            }
+        }
+        $scope.question = noquestion;
+        $scope.prettyStatus = $scope.question.status;
+        console.log("Date has No Question");
+    });
+
+    // TODO: Route to Question View with question data
+    $scope.goToQuestion = function(){
+        console.log("Data to be Sent: ");
+        console.log($scope.question);
+    };
+
+    // Set Today, Options, and Custom Classes
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.options = {
+        customClass: getDayClass,
+        maxDate: new Date(),
+        showWeeks: false
+    };
+
+    function getDayClass(data) {
+        var date = data.date,
+        mode = data.mode;
+        if (mode === 'day') {
+            var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+            for (var i = 0; i < questions.length; i++) {
+                var currentDay = new Date(questions[i].date).setHours(0,0,0,0);
+
+                if (dayToCheck === currentDay) {
+                    return questions[i].status;
+                }
+            }
+        }
+
+        return '';
+    } 
+});
+
+mainApp.controller('questionController', function($scope){
+
+    $scope.questions = questions;
+
+    $scope.model = {
+      questionid: 11,
+      option: ''
+    };
+
+    $scope.$watch('model.questionid', function(){
+
+        for (var i = 0; i < $scope.questions.length; i++) {
+            var currentQuestion = $scope.questions[i];
+
+            if ($scope.model.questionid == currentQuestion.id) {
+                $scope.model.option = '';
+                $scope.question = questions[i];
+                console.log('Question changed to:');
+                console.dir($scope.question);
+
+                return true;
+            }
+        }
+    });
+
+
+    $scope.submitanswer = function(){
+      console.log("Option " + $scope.model.option + " confirmed for Question ID " + $scope.question.id);
+    };
+});
+
+mainApp.controller('askQuestionController', function($scope){
+    $scope.test = 'Ask Question Controller is connected.';
+});
+
+mainApp.controller('classDataController', function($scope){
+    $scope.test = 'Class Data Controller is connected.';
+});
+
+mainApp.controller('accountController', function($scope){
+    $scope.test = 'Account Controller is connected.';
+});
+
+questions = [
+      {
+          id: 11,
+          date: new Date(2016, 5, 14),
+          title: "Which are valid declarations of variables?",
+          concepts: "variables, primitive types",
+          status: 'attemptedCorrect',
+          code: "var bob = 6;\nvar jake = bob;\nsteve = bob + jake;",
+          options: ["hello 1", "Option 2", "goodbye 3", "Option 4"],
+      },
+      {
+          id: 12,
+          date: new Date(2016, 5, 15),
+          title: "Which are valid declarations of variables?",
+          concepts: "variables, primitive types",
+          status: 'attemptedCorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 13,
+          date: new Date(2016, 5, 16),
+          title: "Which are valid declarations of variables?",
+          concepts: "variables, primitive types",
+          status: 'attemptedIncorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 14,
+          date: new Date(2016, 5, 17),
+          title: "Which are valid declarations of variables?",
+          concepts: "variables, primitive types",
+          status: 'attemptedCorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 15,
+          date: new Date(2016, 5, 18),
+          title: "Which are valid declarations of variables?",
+          concepts: "variables, primitive types",
+          status: 'attemptedIncorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 16,
+          date: new Date(2016, 5, 19),
+          title: "Which are valid declarations of variables?",
+          concepts: "variables, primitive types",
+          status: 'attemptedIncorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 17,
+          date: new Date(2016, 5, 20),
+          title: "Which are valid declarations of variables?",
+          concepts: "variables, primitive types",
+          status: 'unattempted',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 18,
+          date: new Date(2016, 5, 21),
+          title: "Which are valid declarations of variables?",
+          concepts: "variables, primitive types",
+          status: 'attemptedIncorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 19,
+          date: new Date(2016, 5, 22),
+          title: "Which are valid declarations of variables?",
+          concepts: "variables, primitive types",
+          status: 'attemptedCorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 110,
+          date: new Date(2016, 5, 23),
+          title: "Which are valid declarations of variables?",
+          concepts: "variables, primitive types",
+          status: 'attemptedCorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 111,
+          date: new Date(2016, 5, 24),
+          title: "Which are valid declarations of variables?",
+          concepts: "variables, primitive types",
+          status: 'attemptedCorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 112,
+          date: new Date(2016, 5, 25),
+          title: "Which is an incorrect constructor?",
+          concepts: "constructors",
+          status: 'attemptedIncorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 113,
+          date: new Date(2016, 5, 26),
+          title: "Which is an incorrect constructor?",
+          concepts: "constructors",
+          status: 'attemptedCorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 114,
+          date: new Date(2016, 5, 27),
+          title: "Which is an incorrect constructor?",
+          concepts: "constructors",
+          status: 'attemptedCorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 115,
+          date: new Date(2016, 5, 28),
+          title: "Which is an incorrect constructor?",
+          concepts: "constructors",
+          status: 'attemptedIncorrect',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 116,
+          date: new Date(2016, 5, 29),
+          title: "Select all proper array instantiations",
+          concepts: "arrays",
+          status: 'unattempted',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 117,
+          date: new Date(2016, 5, 30),
+          title: "Select all proper array instantiations",
+          concepts: "arrays",
+          status: 'unattempted',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      },
+      {
+          id: 118,
+          date: new Date(2016, 6, 1),
+          title: "What is the output of the following code?",
+          concepts: "methods, local variables",
+          status: 'unattempted',
+          code: "",
+          options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+      }
+      ];
--- AdaptiveQ/routes/index.js
+++ AdaptiveQ/routes/index.js
@@ -8,37 +8,37 @@ var localhost = "http://localhost:3000/";
 var sysAccount = 'adaptq@gmail.com';
 
 router.get('/concepts', function(req, res, next){
-  Concepts.getConceptTree(function (tree){
-    res.send(tree);
-  });
+    Concepts.getConceptTree(function (tree){
+        res.send(tree);
+    });
 });
 
 function requireLogin (req, res, next) {
-  if (!req.session.user) {
-    res.redirect('/');
-  } else {
-    next();
-  }
+    if (!req.session.user) {
+        res.redirect('/');
+    } else {
+        next();
+    }
 }
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     if(req.session && req.session.user){
-      // show dashboard here
-      //MeanData = getMean();
-      res.render('layout',{ isTeacher : req.session.isTeacher});
+        // show dashboard here
+        //MeanData = getMean();
+        res.render('layout',{ isTeacher : req.session.isTeacher});
     }
     else
     {
-      res.render('login',{Message: ''});
+        res.render('login',{Message: ''});
     }
 });
 
 router.get('/logout', function(req, res){
-  console.log("Destroying session");
-  req.session.reset();
-  res.redirect('/');
+    console.log("Destroying session");
+    req.session.reset();
+    res.redirect('/');
 });
 
 router.post('/login', function(req, res, next){
@@ -46,142 +46,148 @@ router.post('/login', function(req, res, next){
     var pass = req.body.password;
     console.log(email);
     User.getUserByEmail(email)
-    .then(function (user){
-      if(!user){
-        console.log("Email not found");
-        res.send({'status' : 'ERROR', 'eMessage' : 'Email Not Found!'});
-      }
-      if(user.email){
-        if(user.password == pass){
-		      console.log("User "+user+ "is authenticated!");
-          req.session.user = user;
-          delete req.session.user.records;
-          req.session.startTime = Date.now();
-          req.session.isTeacher = false;
-
-          response = {'status':'OK'};
-          response.data = {};
-          response.data.user = {'id': user._id, 'email' : user.email};
-          if(user.email == "adaptq@gmail.com"){
-            req.session.isTeacher = true;
-            response.data.user.isInstructor = true;
-          }
-          else{
-            response.data.user.isInstructor = false;
-            if(req.session.redirect_to != null){
-              var url = req.session.redirect_to;
-              req.session.redirect_to = null;
-              console.log("redirecting to :"+url);
-              response.data.url = url;
+        .then(function (user){
+            if(!user){
+                console.log("Email not found");
+                res.send({'status' : 'ERROR', 'eMessage' : 'Email Not Found!'});
             }
-          }
-          res.send(response);
-        }
-        else {
-          // TODO password error
-          console.log("Password mismatch");
-          res.send({'status' : 'ERROR', 'eMessage' : 'Incorrect password!'});
-        }
-      }
-    });
-  });
+            if(user.email){
+                if(user.password == pass){
+                    console.log("User "+user+ "is authenticated!");
+                    req.session.user = user;
+                    delete req.session.user.records;
+                    req.session.startTime = Date.now();
+                    req.session.isTeacher = false;
+
+                    response = {'status':'OK'};
+                    response.data = {};
+                    response.data.user = {'id': user._id, 'email' : user.email};
+                    if(user.email == "adaptq@gmail.com"){
+                        req.session.isTeacher = true;
+                        response.data.user.isInstructor = true;
+                    }
+                    else{
+                        response.data.user.isInstructor = false;
+                        if(req.session.redirect_to != null){
+                            var url = req.session.redirect_to;
+                            req.session.redirect_to = null;
+                            console.log("redirecting to :"+url);
+                            response.data.url = url;
+                        }
+                    }
+                    res.send(response);
+                }
+                else {
+                    // TODO password error
+                    console.log("Password mismatch");
+                    res.send({'status' : 'ERROR', 'eMessage' : 'Incorrect password!'});
+                }
+            }
+        });
+});
 
 router.post('/register', function(req, res){
     User.getUserByEmail(req.body.email)
-    .then(function (user){
-      if(user && user.email){
-        // TODO: redirect to error
-        console.log("Found an existing user : " + user);
-      }
-      else{
-        User.createUser(req.body.email, req.body.password, req.body.username)
-        .then(function(savedUser){
-            console.log("Saved: " + savedUser);
-            req.session.user = savedUser;
-            if(savedUser.email != "adaptq@gmail.com"){
-              req.session.isTeacher = false;
+        .then(function (user){
+            if(user && user.email){
+                // TODO: redirect to error
+                console.log("Found an existing user : " + user);
             }
             else{
-              req.session.isTeacher = true;
+                User.createUser(req.body.email, req.body.password, req.body.username)
+                    .then(function(savedUser){
+                        console.log("Saved: " + savedUser);
+                        req.session.user = savedUser;
+                        if(savedUser.email != "adaptq@gmail.com"){
+                            req.session.isTeacher = false;
+                        }
+                        else{
+                            req.session.isTeacher = true;
+                        }
+                        req.session.startTime = Date.now();
+                        res.redirect('/');
+                    }, function (err){
+                        console.log("Error in saving" + newUser +"\n"+err);
+                        // TODO redirect to error
+                    });
             }
-            req.session.startTime = Date.now();
-            res.redirect('/');
-        }, function (err){
-            console.log("Error in saving" + newUser +"\n"+err);
-            // TODO redirect to error
         });
-      }
-    });
-  });
+});
 
-  router.post('/recover', function(req, res){
+router.post('/recover', function(req, res){
     User.getUserByEmail(req.body.recoveryEmail)
-      .then(function (user){
-        if(!user){
-          res.send({'status' : 'ERROR', 'eMessage' : 'No such email found'});
-        }
-        else{
-          user.resetPasswordToken = randomstring.generate();
-          user.save().then(function (savedUser){
-            console.log(savedUser.resetPasswordToken);
-            var url = localhost + "resetPaswword?token=" + savedUser.resetPasswordToken;
-            emailer.sendResetPasswordLink(sysAccount,user.email,url,function(error, message){
-              if(error){
-                console.log(error);
-                res.send({status : 'ERROR', eMessage : 'Error sending email'});
-              }
-              else{
-                console.log(message);
-                res.send({status : 'OK', message : 'Check registered email for link to reset'});
-              }
-            });
+        .then(function (user){
+            if(!user){
+                res.send({'status' : 'ERROR', 'eMessage' : 'No such email found'});
+            }
+            else{
+                user.resetPasswordToken = randomstring.generate();
+                user.save().then(function (savedUser){
+                    console.log(savedUser.resetPasswordToken);
+                    var url = localhost + "resetPaswword?token=" + savedUser.resetPasswordToken;
+                    emailer.sendResetPasswordLink(sysAccount,user.email,url,function(error, message){
+                        if(error){
+                            console.log(error);
+                            res.send({status : 'ERROR', eMessage : 'Error sending email'});
+                        }
+                        else{
+                            console.log(message);
+                            res.send({status : 'OK', message : 'Check registered email for link to reset'});
+                        }
+                    });
 
-          });
-        }
-      }, function(err){
-        console.log("No such email found");
-      });
-  });
+                });
+            }
+        }, function(err){
+            console.log("No such email found");
+        });
+});
 
-  router.get('/resetPaswword', function(req, res){
+router.get('/resetPaswword', function(req, res){
     var token = req.query.token;
     console.log(token);
     User.getUserForToken(token)
-      .then(function(user){
-        if(user){
-          res.render('pwrecovery',{userEmail: user.email, userToken: token});
-        }
-        else{
-          res.send({status: 'ERROR', eMessage: 'Invalid Token'});
-        }
-      }, function(error){
-        console.log(error);
-      });
-  });
+        .then(function(user){
+            if(user){
+                res.render('pwrecovery',{userEmail: user.email, userToken: token});
+            }
+            else{
+                res.send({status: 'ERROR', eMessage: 'Invalid Token'});
+            }
+        }, function(error){
+            console.log(error);
+        });
+});
 
-  router.post('/resetPassword', function(req, res){
+router.post('/resetPassword', function(req, res){
     var userEmail = req.body.email;
     var password = req.body.password;
     var token = req.body.token;
     if(!token || !userEmail || !password){
-      res.send({status: 'ERROR', eMessage: 'Required Parameters not provided'});
+        res.send({status: 'ERROR', eMessage: 'Required Parameters not provided'});
     }
     User.getUserForToken(token)
-    .then(function(user){
-      if(user && userEmail == user.email){
-        user.password = password;
-        user.resetPasswordToken = "";
-        user.save()
-          .then(function(user){
-            res.send({status: 'OK', message: 'Password succesfuly reset', url : '/'});
-          });
-      }
-      else{
-        res.send({status: 'ERROR', eMessage: 'Invalid Token for emailId'});
-      }
-    }, function(error){
-      console.log(error);
-    });
-  });
+        .then(function(user){
+            if(user && userEmail == user.email){
+                user.password = password;
+                user.resetPasswordToken = "";
+                user.save()
+                    .then(function(user){
+                        res.send({status: 'OK', message: 'Password succesfuly reset', url : '/'});
+                    });
+            }
+            else{
+                res.send({status: 'ERROR', eMessage: 'Invalid Token for emailId'});
+            }
+        }, function(error){
+            console.log(error);
+        });
+});
+
+router.get('/partials/:name', function (req, res){
+    var name = req.params.name;
+    console.log('partials/' + name);
+    res.render('partials/' + name);
+});
 
 module.exports = router;
